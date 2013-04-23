d5dd6d23686d079d8f8f55f5ca595a134acb8a95
Chris Rose <chrisroseuk@gmail.com>
2013-04-23 14:48:13 +0100

    Updated Registration View

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130423123045) do
+ActiveRecord::Schema.define(:version => 20130423123429) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20130423123045) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "telephone"
+    t.string   "fullname"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
