bf41f83de343dec3af21f2455eebb7274bfe3dac
Chris Rose <chrisroseuk@gmail.com>
2013-04-23 13:35:12 +0100

    Added Name and Telephone Fields to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130421220223) do
+ActiveRecord::Schema.define(:version => 20130423123045) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20130421220223) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "telephone"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
